d95568f281d3be7fa1ded3336759fcdeabe9facb
NiceSiqing <siqingxu@umich.edu>
2025-07-29 18:11:46 +0800

    feat: products complete

--- src/assets/products.js
+++ src/assets/products.js
@@ -3,7 +3,7 @@ const products = [
     id: 1,
     name: "Honey Garlic",
     description: "Brushed with a sweet, soy-based sauce, these are light on heat and heavy on flavor.",
-    image: "",
+    image: "./HoneyGarlic.png",
     spiceLevel: 1,
     fav: true
   },
@@ -11,7 +11,7 @@ const products = [
     id: 2,
     name: "Golden Original",
     description: "Korean fried chicken at its best. Juicy and tender inside, light and crispy outside.",
-    image: "",
+    image: "./Golden-Original-1.png",
     spiceLevel: 0,
     fav: true
   },
@@ -19,7 +19,7 @@ const products = [
     id: 3,
     name: "Secret Sauce",
     description: "A sweet and tangy take on our Golden Original.",
-    image: "",
+    image: "./Secret-Sauce-1.png",
     spiceLevel: 1,
     fav: false
   },
@@ -27,7 +27,7 @@ const products = [
     id: 4,
     name: "Hot Spicy",
     description: "A gochujang sauce gives these serious heat, and serious flavor.",
-    image: "",
+    image: "./Hot-Spicy-1.png",
     spiceLevel: 3,
     fav: false
   },
@@ -35,7 +35,7 @@ const products = [
     id: 5,
     name: "Soy Garlic",
     description: "Served in a savory sauce with a mild tang of garlic.",
-    image: "",
+    image: "./SoyGarlic.png",
     spiceLevel: 1,
     fav: true
   },
@@ -43,7 +43,7 @@ const products = [
     id: 6,
     name: "Galbi",
     description: "A Korean classic. Sweet, savory, and smoky with green onions and sesame seed garnish.",
-    image: "",
+    image: "./Galbi.png",
     spiceLevel: 0,
     fav: false
   },
@@ -51,7 +51,7 @@ const products = [
     id: 7,
     name: "Spicy Galbi",
     description: "Tastes like regular Galbi, but with an extra smoky, extra spicy finish.",
-    image: "",
+    image: "./SpicyGalbi_Wings_45.png",
     spiceLevel: 2,
     fav: false
   },
@@ -59,7 +59,7 @@ const products = [
     id: 8,
     name: "Caribbean Spice",
     description: "The sauce of the summer. Try this tropical sensation today. Crispy chicken slathered in our special Caribbean Jerk Sauce.",
-    image: "",
+    image: "./Caribbean-Spicy.png",
     spiceLevel: 2,
     fav: true
   },
@@ -67,7 +67,7 @@ const products = [
     id: 9,
     name: "Cheesling",
     description: "Dusted in a rich medley of sweet cheeses. Grab extra napkins.",
-    image: "",
+    image: "./Cheesling_Boneless.png",
     spiceLevel: 0,
     fav: false
   },
@@ -75,7 +75,7 @@ const products = [
     id: 10,
     name: "Gangnam Style",
     description: "A black pepper sauce gives these some heat, but the garlic and onions balance it out well.",
-    image: "",
+    image: "./GangnamStyle_Wings.png",
     spiceLevel: 1,
     fav: false
   },
@@ -83,7 +83,7 @@ const products = [
     id: 11,
     name: "Gang-Jeong",
     description: "A soy-based sauce with extra spice thanks to a cinnamon blend and chili garnish.",
-    image: "",
+    image: "./GangJeong.png",
     spiceLevel: 1,
     fav: false
   },
@@ -91,7 +91,7 @@ const products = [
     id: 12,
     name: "Spicy Original",
     description: "Light and crispy with an added kick.",
-    image: "",
+    image: "./SpicyGoldenOriginal_Wings_45.png",
     spiceLevel: 3,
     fav: false
   },
@@ -99,7 +99,7 @@ const products = [
     id: 13,
     name: "Wings of Fire",
     description: "Korea's hottest wings. Order if you dare.",
-    image: "",
+    image: "./WingsOfFire_Wings_45.png",
     spiceLevel: 4,
     fav: false
   },
@@ -107,7 +107,7 @@ const products = [
     id: 14,
     name: "Hot Mala",
     description: "Inspired by Chinese mala seasoning, this flavor is very sweet and very hot.",
-    image: "",
+    image: "./HotMala.png",
     spiceLevel: 3,
     fav: false
   }
